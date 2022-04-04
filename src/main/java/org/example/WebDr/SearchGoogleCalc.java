ece522db0dfd31a099f75fa23498a8e61c307b32
Radmir <gerasimoffdg@gmail.com>
2022-04-04 13:47:14 +0300

    Переделал по простому. С Page Object знаком, но по реализации чутка непонятен

--- src/main/java/org/example/WebDr/SearchGoogleCalc.java
+++ src/main/java/org/example/WebDr/SearchGoogleCalc.java
@@ -3,16 +3,14 @@ package org.example.WebDr;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchGoogleCalc {
     private static WebDriver driver;
 
     public static void main(String[] args) throws InterruptedException {
         driver = new ChromeDriver();
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
-        Actions actions = new Actions(driver);
+//        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+//        Actions actions = new Actions(driver);
 
         driver.get("https://google.com");
         Thread.sleep(5000);
--- src/main/java/org/example/WebDr/TestingForSS.java
+++ src/main/java/org/example/WebDr/TestingForSS.java
@@ -1,42 +1,64 @@
 package org.example.WebDr;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestingForSS {
-    private static WebDriver driver;
+    public static WebDriver driver;
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
        driver = new ChromeDriver();
-//        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+//       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-//        driver.get("https://google.com");
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
 
-//        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.name("div[@jsname='vdLsw']")));
-//        driver.findElement(By.id("div[@jsname='vdLsw']")).click();
+        driver.get("https://google.com");
+        Thread.sleep(5000);
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='gLFyf gsfi']")));
+        driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).click();
+//        Thread.sleep(3000);
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='gLFyf gsfi']")));
+        driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("Калькулятор");
 //        Thread.sleep(5000);
 
-//        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("div[@jsname='vdLsw']")));
- //       driver.findElement(By.name("div[@jsname='vdLsw']")).sendKeys("Калькулятор");
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='gNO89b']")));
+        driver.findElement(By.xpath("//input[@class='gNO89b']")).click();
+//        Thread.sleep(10000);
 
-//        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("div[@class='CcAdNb']")));
-//        driver.findElement(By.id("div[@class='CcAdNb']")).click();
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='N10B9']")));
+        driver.findElement(By.xpath("//div[@jsname='N10B9']")).click();
 
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='YovRWb']")));
+        driver.findElement(By.xpath("//div[@jsname='YovRWb']")).click();
 
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='lVjWed']")));
+        driver.findElement(By.xpath("//div[@jsname='lVjWed']")).click();
 
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='pPHzQc']")));
+        driver.findElement(By.xpath("//div[@jsname='pPHzQc']")).click();
 
-//        driver.quit();
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='KN1kY']")));
+        driver.findElement(By.xpath("//div[@jsname='KN1kY']")).click();
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='XSr6wc']")));
+        driver.findElement(By.xpath("//div[@jsname='XSr6wc']")).click();
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='N10B9']")));
+        driver.findElement(By.xpath("//div[@jsname='N10B9']")).click();
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@jsname='Pt8tGc']")));
+        driver.findElement(By.xpath("//div[@jsname='Pt8tGc']")).click();
+        Thread.sleep(5000);
+
+        driver.quit();
 
     }
-
-//    public static void transToCalc() throws InterruptedException {
-//        driver.get("https://google.com");
-//        Thread.sleep(5000);
-//        driver.findElement(By.id("input[@class='gLFyf gsfi']")).sendKeys("Калькулятор");
-//        driver.findElement(By.xpath("span[@class='QCzoEc z1asCe MZy1Rb']")).click();
-//    }
-
 
 }
